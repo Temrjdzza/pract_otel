b92de72d705d744bc9e536accc4303f036dd9b11
i-am-ZhVi <unius6ls66@gmail.com>
2025-06-16 01:11:40 +0300

    create info page

---
+++ dashboard/otel_info.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+
+        <link rel="stylesheet" href="stylesheets/clear-stock.css" />
+        <link rel="stylesheet" href="stylesheets/info_page.css" />
+    </head>
+
+    <body>
+        <?php include "components/header.html"; ?>
+
+        <section class="mini-info">
+            <div class="location">
+                <h3>
+                    Расположение: Дальний восток, промышленный проспект, первая
+                    улица, дом последний
+                </h3>
+            </div>
+        </section>
+
+        <section class="main">
+            <ul>
+                <li>
+                    <div>
+                        <h3>
+                            Большое количество номеров размером - достаточно
+                            квадратных метров
+                        </h3>
+                    </div>
+                    <img src="images/room.jpg" />
+                </li>
+                <li>
+                    <div>
+                        <h3>Достаточное количество комнат ожиданий</h3>
+                    </div>
+                    <img src="images/notroom.jpg" />
+                </li>
+                <li>
+                    <div>
+                        <h3>
+                            Бассейн в зании обьемом - достаточно куб. метров
+                        </h3>
+                    </div>
+                    <img src="images/pool_inside.jpg" />
+                </li>
+                <li>
+                    <div>
+                        <h3>Бассейн вне здания обьемом - много куб. метров</h3>
+                    </div>
+                    <img src="images/pool_outside.jpg" />
+                </li>
+            </ul>
+        </section>
+
+        <?php include "components/footer.html"; ?>
+    </body>
+</html>
