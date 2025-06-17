6167d739e7c538745cff4bacbfb5ed810e911f62
i-am-ZhVi <unius6ls66@gmail.com>
2025-06-18 02:39:33 +0300

    create rooms page

---
+++ dashboard/rooms.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+
+        <link rel="stylesheet" href="stylesheets/clear-stock.css" />
+        <link rel="stylesheet" href="stylesheets/rooms_page.css" />
+        <link rel="stylesheet" href="stylesheets/slider.css" />
+    </head>
+
+    <body>
+        <?php include "components/header.html"; ?>
+
+        <section class="tab">
+            <div class="tab-div">
+                <button class="sort">Сортировать</button>
+                <button class="filter">Фильтры</button>
+            </div>
+        </section>
+
+        <section class="main">
+            <ul class="list-rooms">
+                <?php include "components/room.html"; ?>
+            </ul>
+        </section>
+
+        <?php include "components/footer.html"; ?>
+    </body>
+</html>
